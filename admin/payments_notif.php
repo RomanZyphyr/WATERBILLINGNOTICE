b788c130dd4a09584e9c15f2a03692c7ab94e80f
MiahPat <144814160+RomanZyphyr@users.noreply.github.com>
2024-06-12 22:48:54 +0800

    Create payments_notif.php

---
+++ admin/payments_notif.php
@@ -0,0 +1,18 @@
+<div class="modal fade" id="messageModal1" tabindex="-1"
+                                aria-labelledby="messageModalLabel1" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content notif">
+                                        <div class="modal-header ">
+                                            <h5 class="modal-title text-info" id="messageModalLabel1">Notification
+                                            </h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="text-info fs-5" id="messageModalBody1">
+                                            </div>
+                                            <!-- Message will be inserted here -->
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
