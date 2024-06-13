109726b72bb140bf33258602b2cd9ed8ea5c3f45
MiahPat <165100522+febC123@users.noreply.github.com>
2024-06-13 23:36:28 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -989,7 +989,24 @@
         }
     });
 </script>
+ <script>
+        document.getElementById('Utoggle-password').addEventListener('click', function() {
+            const passwordField = document.getElementById('Upassword');
+            const icon = this;
 
+            if (passwordField.type === 'password') {
+                passwordField.type = 'text';
+                icon.classList.remove('bi-eye-slash');
+                icon.classList.add('bi-eye');
+            } else {
+                passwordField.type = 'password';
+                icon.classList.remove('bi-eye');
+                icon.classList.add('bi-eye-slash');
+            }
+        });
+    </script>
+
+    <!-- script for dashboard -->
 
 
 </body>
