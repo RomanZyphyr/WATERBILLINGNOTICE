f17dfb74e711d75041544daf7a85d85de13ed72a
MiahPat <165100522+febC123@users.noreply.github.com>
2024-06-13 23:34:33 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -956,6 +956,40 @@
         });
     </script>
     
+     <!-- script for dashboard -->
+
+
+<script>
+    document.getElementById('toggle-password').addEventListener('click', function() {
+        const passwordField = document.getElementById('Cpassword');
+        const icon = this;
+
+        if (passwordField.type === 'password') {
+            passwordField.type = 'text';
+            icon.classList.remove('bi-eye-slash');
+            icon.classList.add('bi-eye');
+        } else {
+            passwordField.type = 'password';
+            icon.classList.remove('bi-eye');
+            icon.classList.add('bi-eye-slash');
+        }
+    });
+    document.getElementById('updatespass').addEventListener('click', function() {
+        const passwordField = document.getElementById('Updatepassword');
+        const icon = this;
+
+        if (passwordField.type === 'password') {
+            passwordField.type = 'text';
+            icon.classList.remove('bi-eye-slash');
+            icon.classList.add('bi-eye');
+        } else {
+            passwordField.type = 'password';
+            icon.classList.remove('bi-eye');
+            icon.classList.add('bi-eye-slash');
+        }
+    });
+</script>
+
 
 
 </body>
