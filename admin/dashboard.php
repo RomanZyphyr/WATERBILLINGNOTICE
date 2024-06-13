5410d7fc9eed44e65aae6f1eb70930a336f6c6c6
MiahPat <165100522+febC123@users.noreply.github.com>
2024-06-13 23:30:07 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -896,6 +896,18 @@
     </script>
 
     
+<script>
+        function showContent(event) {
+            event.preventDefault();
+            // Hide all main content sections
+            document.querySelectorAll('.main-content').forEach(section => {
+                section.classList.add('d-none');
+            });
+            // Show the target main content section
+            const target = event.target.closest('a').getAttribute('data-target');
+            document.querySelector(target).classList.remove('d-none');
+        }
+    </script>
 
 
 </body>
